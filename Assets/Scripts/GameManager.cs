2d3eb4d01b06337c36b2beffc17e7d8f7a25f836
YakumoOhtagaki <yakumo.ohtagaki@techouse.jp>
2015-10-10 15:56:31 +0900

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+	
+	private const string NORMAL_HIGH_SCORE_KEY = "normalHighScore";
+	private const string NORMAL_LAST_SCORE_KEY = "normalLastScore";
+	public static int highScore;
+	public static int lastScore;
+	public static float score;
+
+	[SerializeField] private float scoreMagnification = 10.0f;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	public void GameInit(){
+		score = 0;
+		highScore = PlayerPrefs.GetInt (NORMAL_HIGH_SCORE_KEY, 0);
+		lastScore = PlayerPrefs.GetInt (NORMAL_LAST_SCORE_KEY, 0);
+		
+		//init
+	}
+	
+	public void GameStart(){
+		//start
+	}
+	
+	public void GameFinish(){
+		/*
+		if (CanvasManager.currentStatus == CanvasManager.Status.Game) {
+			AudioManager.Instance.PlaySE("gameover");
+			if (score > highScore) {
+				PlayerPrefs.SetInt (NORMAL_HIGH_SCORE_KEY, (int)score);
+				PlayerPrefs.Save ();
+			}
+			PlayerPrefs.SetInt (NORMAL_LAST_SCORE_KEY, (int)score);
+			PlayerPrefs.Save ();
+			CanvasManager.currentStatus = CanvasManager.Status.ResultInit;
+		}
+		*/
+	}
+	
+	private void AddScore(float value){
+		score += - value * scoreMagnification;
+		if (score < 0) {
+			score = 0;
+		}
+	}
+	
+	public void Awake()	{
+		base.Awake ();
+
+		DontDestroyOnLoad(this.gameObject);
+
+		Application.targetFrameRate = 60;
+	}
+	
+}
---
+++ Assets/Scripts/TyphoonController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TyphoonController : MonoBehaviour {
+
+	private float density;	//密度
+	private float hp;		//勢力
+	private Vector2 point;	//座標
+	private float size;		//サイズ
+
+	// Use this for initialization
+	void Start () {
+		point = new Vector2 (100 / 2, 100);	//画面サイズ半分初期値
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Init(){
+
+	}
+
+	public void Move(float value){
+		//if移動量限界
+		point.x += value;
+	}
+
+	public void AddDensity(float value){
+		//if-限界
+		density += value;
+	}
+
+	public void AddHP(float value){
+		//if-限界
+		hp += value;
+	}
+
+
+
+
+
+
+}
