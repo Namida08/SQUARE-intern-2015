45b56edbccb35483e4bc559f6f5b5074b641fba3
Namida08 <namida008@gmail.com>
2015-10-12 10:54:45 +0900

    input

---
+++ Assets/Scripts/AutoParticle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoParticle : MonoBehaviour
+{
+	ParticleSystem myParticleSystem;
+	
+	void Awake (){
+		myParticleSystem = this.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (myParticleSystem != null && myParticleSystem.particleCount == 0){
+			Debug.Log ("Zero");
+		}
+	}
+}
